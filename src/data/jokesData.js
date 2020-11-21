acd9f28a4252a24f602bacc8e2a5c4d5648cef71
dannebernhardt199610 <danielbernhardt1996@outlook.com>
2020-11-21 16:30:36 +0100

    first-commit

---
+++ src/data/jokesData.js
@@ -0,0 +1,25 @@
+const JokesData = [
+    {
+        id: 1, 
+        question: "Whats best about sweden?",
+        answer: "Dagges vidriga lägga"
+    },
+
+    {
+        id: 2, 
+        question: "Who is the sexiest guy in sweden?",
+        answer: "Daniel Bernhardt"
+    },
+    {
+        id: 3, 
+        question: "What are you doing right now?",
+        answer: "Learning React"
+    },
+    {
+        id: 4, 
+    
+        answer: "Svar utan fråga"
+    }
+]
+
+export default JokesData
