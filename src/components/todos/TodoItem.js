dbf52e3ac47880fc758b4dfed75ff97d0bd2f99c
Martin <martin@xkmf.se>
2020-11-23 20:41:03 +0100

    fix bug in selecting todo item

--- src/components/todos/TodoItem.js
+++ src/components/todos/TodoItem.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, {Component} from "react"
 
-function TodoItem(props) {
-    const completedStyle = {
-        fontStyle: "italic",
-        color: "#cdcdcd",
-        textDecoration: "line-through"
-    }
 
-    return (
-    <div className="todo-item">
-        <input type="checkbox" checked={props.item.completed}
-        onChange={ () => props.handleChange(props.item.id) }
-        />
-        
-        <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
-        
-    </div>
+class TodoItem extends Component {
+  constructor(props){
+    super();
+    this.state = {
+        id: props.item.id,
+        text: props.item.text,
+        completed: props.item.completed,
+    };
+
+    this.clicked = this.clicked.bind(this);
+  }
+
+  clicked(){
+      this.setState({
+        completed: !this.state.completed
+      })
+  }
+
+  render(){
+    return(
+      <div className="todo-item">
+      <input type="checkbox" checked={this.state.completed} onChange={this.clicked}/>
+      <p style={this.state.completed?({
+          fontStyle: "italic",
+          color: "#cdcdcd",
+          textDecoration: "line-through"
+        }):({})}>{this.state.text}</p>
+      </div>
     )
+  }
 }
 
-export default TodoItem
+export default TodoItem;
--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -10,38 +10,16 @@ class Todos extends Component {
     this.state = {
         todos: todosData
     }
-    this.handleChange = this.handleChange.bind(this)
 }
 
-
-handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
-        if(todo.id === id) {
-          todo.completed = !todo.completed
-        }
-        return todo
-      })
-      return {
-        todos: updatedTodos
-      }
-    })
+render() {
+  return (
+      <div
+      className="todo-list"> {this.state.todos.map(item => <TodoItem key={item.id} item={item} />)}
+      </div>
+    )
   }
-
-  
-
-        render() {
-            const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
-
-            return (
-                <div 
-                className="todo-list"> {todoItems} 
-                </div>
-               
-            )
-       
-        }
-    }
+}
 
 
 export default Todos
